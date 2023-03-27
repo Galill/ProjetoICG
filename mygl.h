5bc318d23077a8e0a0d52de59548a22eb3ce4ca8
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2023-03-26 22:19:43 -0300

    Update mygl.h

--- mygl.h
+++ mygl.h
@@ -1,9 +1,7 @@
 #ifndef _MYGL_H_
 #define _MYGL_H_
-#endif
 
 #include "definitions.h"
-#include <GL/glew.h>
 
 //-----------------------------------------------------------------------------
 static void MyGlDraw(void);
@@ -11,3 +9,5 @@ static void MyGlDraw(void);
 static void PutPixel(void);
 static void DrawLine(void);
 static void DrawTriangle(void);
+
+#endif
